feabad76dcf6ed62a89ef4676f06b3be51d86397
hazedrops <hazedrops@hotmail.com>
2025-07-21 22:00:48 -0700

    product-model.js file added, so model viewer template is open in a new window

---
+++ assets/product-model.js
@@ -0,0 +1,47 @@
+let instanceCount = 0
+
+class ProductModel extends HTMLElement {
+  constructor() {
+    super()
+
+    instanceCount++
+    console.log(`Creating ProductModel instance #${instanceCount}`)
+
+    this.openModelModal()
+  }
+
+  getMediaID() {
+    const id = this.getAttribute("data-media-id")
+    return id
+  }
+
+  getModal() {
+    const modal = document.getElementById("productModelModal")
+    return modal
+  }
+
+  openModelModal() {
+    const mediaID = this.getMediaID()
+    const modal = this.getModal()
+
+    if (!mediaID) return
+
+    const openModalButton = this.querySelector(
+      `button[id="productModelOpenButton_${mediaID}"]`
+    )
+    openModalButton.addEventListener("click", function (e) {
+      modal.querySelector("#body").innerHTML = ""
+
+      const template = document.querySelector(
+        `product-model[data-media-id="${mediaID}"] > template`
+      )
+
+      const clone = template.content.cloneNode(true)
+
+      modal.querySelector("#body").appendChild(clone)
+      modal.querySelector("#body > model-viewer").setAttribute("reveal", "auto")
+    })
+  }
+}
+
+const productModel = customElements.define("product-model", ProductModel)
